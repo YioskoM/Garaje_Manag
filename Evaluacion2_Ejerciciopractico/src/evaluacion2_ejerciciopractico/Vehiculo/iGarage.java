399141bf5de9fc1dd6995c002cd6e767af59724a
Karen_cruz <u20241220955@usco.edu.co>
2024-11-22 23:36:22 -0500

    Delete Evaluacion2_Ejerciciopractico directory

--- Evaluacion2_Ejerciciopractico/src/evaluacion2_ejerciciopractico/Vehiculo/iGarage.java
+++
@@ -1,5 +0,0 @@
-package evaluacion2_ejerciciopractico.Vehiculo;
-public interface iGarage {
-    public double calcularIngresos();
-    public int calcularOcupacionPorTipoVehiculo(Class<?> tipoVehiculo);
-}
