6aad6dcff3a00d1eb8db13631470af9faddcba01
Diego Gutierrez <u20241221440@usco.edu.co>
2024-11-24 11:36:52 -0500

    create getters and setters

--- Evaluacion2_Ejerciciopractico/src/evaluacion2_ejerciciopractico/Garajes/Garaje.java
+++ Evaluacion2_Ejerciciopractico/src/evaluacion2_ejerciciopractico/Garajes/Garaje.java
@@ -31,6 +31,78 @@ public class Garaje implements iGarage {
         espacios = new ArrayList<>();
     }
 
+    //! Crear getters and setters COMMIT OPEN#3
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public String getNameAdmin() {
+        return nameAdmin;
+    }
+
+
+
+    public void setNameAdmin(String nameAdmin) {
+        this.nameAdmin = nameAdmin;
+    }
+
     @Override
     public double calcularIngresos() {
         double ingresos = 0;
