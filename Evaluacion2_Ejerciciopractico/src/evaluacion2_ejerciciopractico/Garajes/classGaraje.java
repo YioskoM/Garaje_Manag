daa04e74beacec22e122aee03c2e52d44dbc7a0f
Diego Gutierrez <u20241221440@usco.edu.co>
2024-11-24 11:08:42 -0500

    crear classGaraje atribute

--- Evaluacion2_Ejerciciopractico/src/evaluacion2_ejerciciopractico/Garajes/classGaraje.java
+++ Evaluacion2_Ejerciciopractico/src/evaluacion2_ejerciciopractico/Garajes/classGaraje.java
@@ -1,1 +1,12 @@
 package evaluacion2_ejerciciopractico.Garajes;
+
+
+//! Crear la classe Garaje con los atributos COMMIT OPEN#1      
+public class classGaraje {
+    private String departamento;
+    private String ciudad;
+    private String direccion;
+    private int numero;
+    private String email;
+    private String nameAdmin;
+}
